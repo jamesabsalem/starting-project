7041dbb1caa748e3b9900519969a0e30185e913d
James <jamesabsalem@gmail.com>
2025-03-08 12:38:49 +0600

    card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, CardComponent],
   imports: [BrowserModule, UserComponent, TasksComponent],
   bootstrap: [AppComponent],
 })
